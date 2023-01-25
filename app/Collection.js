5a28e8f72e6ac34e67edaa5591b85592d9552632
Guillermo Rodrigo Palacios Soriano <guillerps1900@gmail.com>
2023-01-24 20:18:43 -0600

    basic functionality

--- app/Collection.js
+++ app/Collection.js
@@ -1,8 +1,15 @@
+"use client"
+
 import React from 'react'
 
 export default function Collection({element}) {
+    const handleClick = () => {
+        
+    }
+
+
   return (
-    <div className='border-2 border-red-500 px-4 rounded-lg py-2 basis-1/5 overflow-hidden'>
+    <div className='border-2 border-red-500 px-4 rounded-lg py-2 basis-1/5 overflow-hidden' onClick={handleClick}>
         <div>{element.title}</div> 
         <div>{element.task}</div> 
     </div>
